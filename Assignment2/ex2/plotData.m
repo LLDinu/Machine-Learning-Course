1c20bff3f2bf2fb1ceaaefe693c1fde70ee9be23
LLDinu <37257846+LLDinu@users.noreply.github.com>
2018-03-11 15:01:59 +0800

    Add files via upload

---
+++ Assignment2/ex2/plotData.m
@@ -0,0 +1,28 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+pos0=find(y==0);
+pos1=find(y==1);
+plot(X(pos0,1),X(pos0,2),'ko','MarkerSize',7,'MarkerFaceColor','y');
+plot(X(pos1,1),X(pos1,2),'k+', 'LineWidth',2,'MarkerSize',7);
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
